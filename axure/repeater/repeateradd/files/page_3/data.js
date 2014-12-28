530726d218931172b911005f732f90d0a3e2aaba
ow-wo <dongjunjie2007@gmail.com>
2014-12-28 20:40:36 +0800

    add axure repeateradd

---
+++ axure/repeater/repeateradd/files/page_3/data.js
@@ -0,0 +1,38 @@
+﻿$axure.loadCurrentPage({
+  "url":"page_3.html",
+  "generationDate":new Date(1419762108425.62),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable",
+"VarNum",
+"VarXuehao"],
+  "page":{
+    "packageId":"925f1b9cc7554ad193a6a6c45da367be",
+    "type":"Axure:Page",
+    "name":"Page 3",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"near",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFFFFFFFF},
+      "image":null,
+      "imageHorizontalAlignment":"near",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"auto",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"Applied Font",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+},
+    "diagram":{
+      "objects":[]}},
+  "masters":{
+},
+  "objectPaths":{
+}});
