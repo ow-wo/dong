1e1e68e80894fe06356cdac88d24726abf30b122
ow-wo <dongjunjie2007@gmail.com>
2014-12-28 20:45:45 +0800

    add axure repeater delete filter search sort

---
+++ axure/repeater/repeatersearch/files/page_2/data.js
@@ -0,0 +1,38 @@
+﻿$axure.loadCurrentPage({
+  "url":"page_2.html",
+  "generationDate":new Date(1419762869569.16),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable",
+"VarNum",
+"VarXuehao"],
+  "page":{
+    "packageId":"a731899b8940458dbd5a8d84a7481c04",
+    "type":"Axure:Page",
+    "name":"Page 2",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"near",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFFFFFFFF},
+      "image":null,
+      "imageHorizontalAlignment":"near",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"auto",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"Applied Font",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+},
+    "diagram":{
+      "objects":[]}},
+  "masters":{
+},
+  "objectPaths":{
+}});
